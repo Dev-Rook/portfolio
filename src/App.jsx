b799ddd980adccf2fa9683cec92e1e0c14179d84
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-19 15:18:56 -0500

    Upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Styles from "./App.module.css";
 
 // import Sidebar from "./Components/_Sidebar/Sidebar";
+import Nav from "./Components/_Nav/Nav";
 
 function App() {
   return (
     <div className={Styles.App}>
-
+      <Nav />
     </div>
   );
 }
--- src/Components/_Nav/Nav.jsx
+++ src/Components/_Nav/Nav.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import Styles from "./Styles/Nav.module.css";
+
+import CloseIcon from "@mui/icons-material/Close";
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+  const [menu, setMenu] = useState(false);
 
-export default Nav
+  const toogleMenu = () => {
+    setMenu((prev) => !prev);
+  };
+
+  return (
+    <nav className={Styles.Nav}>
+      <img
+        src={require("../../Assets/Icons/Menu-Icon.png")}
+        alt=""
+        onClick={toogleMenu}
+        className={Styles.Nav_Icon}
+      />
+
+      <div className={`${Styles.Mobile_Menu} ${menu ? Styles.ShowMenu : ""}`}>
+        <div className={Styles.Header}>
+          <CloseIcon className={Styles.CloseIcon} sx={{fontSize: 30}} onClick={toogleMenu}/>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
