da0f0b4969bc6714b7492a9664d2bd71f933891a
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-21 16:57:46 -0500

    Portfolio Button

--- src/Views/Landing/Landing.jsx
+++ src/Views/Landing/Landing.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Styles from "./Styles/Landing.module.css";
 
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+
 import Hero from "../Hero/Hero";
 import Featured from "../../Components/Project-Data/Featured/Featured";
 import Other from "../../Components/Project-Data/Other/Other";
@@ -12,6 +14,11 @@ const Landing = () => {
       <div className={Styles.Slider_Container}>
         <Featured />
         <Other />
+
+        <button className={Styles.Portfolio_Button}>
+          Portfolio
+          <KeyboardArrowRightIcon />
+        </button>
       </div>
     </div>
   );
