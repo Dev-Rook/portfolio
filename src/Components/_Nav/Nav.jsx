e894bee8fb128e828cc2c09e0fe46db9ed787347
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-22 19:47:10 -0500

    Mobile Menu Progress

--- src/Components/_Nav/Nav.jsx
+++ src/Components/_Nav/Nav.jsx
@@ -68,8 +68,13 @@ const Nav = () => {
       </div>
 
       <div className={`${Styles.Mobile_Menu} ${menu ? Styles.ShowMenu : ""}`}>
+        
         <div className={Styles.Header}>
         <img src={require("../../Assets/Icons/Logo.png")} alt="" className={Styles.Logo} />
+
+        <h1 className={Styles.Large_Text}>
+          Daniel <span className={Styles.Red_R}>R</span>ook
+        </h1>
 
           <CloseIcon
             className={Styles.CloseIcon}
@@ -78,56 +83,31 @@ const Nav = () => {
           />
         </div>
 
-        <div className={Styles.Profile_Section}>
-          <HeroSlider />
-        </div>
+        <ul className={Styles.Navlink_Container}>
+          <li className={Styles.NavItem}>
+            <a href="" className={Styles.Navlink}>
+              Home
+            </a>
+          </li>
+          <li className={Styles.NavItem}>
+            <a href="" className={Styles.Navlink}>
+              Portfolio
+            </a>
+          </li>
+          <li className={Styles.NavItem}>
+            <a href="" className={Styles.Navlink}>
+              Post
+            </a>
+          </li>
+          <li className={Styles.NavItem}>
+            <a href="" className={Styles.Navlink}>
+              Contact
+            </a>
+          </li>
+        </ul>
 
-        <div className={Styles.MenuLink_Container}>
-          <Accordion
-            expanded={expanded === "panel1"}
-            onChange={handleChange("panel1")}
-            className={Styles.Accordion}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1bh-content"
-              id="panel1bh-header"
-            >
-              <Typography sx={{ width: "33%", flexShrink: 0 }}>
-                Portfolio
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
-                feugiat. Aliquam eget maximus est, id dignissim quam.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion
-            expanded={expanded === "panel4"}
-            onChange={handleChange("panel4")}
-            className={Styles.Accordion}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel4bh-content"
-              id="panel4bh-header"
-            >
-              <Typography sx={{ width: "33%", flexShrink: 0 }}>
-                Contact
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
-                Integer sit amet egestas eros, vitae egestas augue. Duis vel est
-                augue.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </div>
-        <Featured />
+
+        {/* <Featured /> */}
 
       </div>
     </nav>
