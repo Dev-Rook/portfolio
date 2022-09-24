40632c95a44a86cddd0f3e9f36b1e0e415e78f19
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-23 21:20:29 -0500

    m

--- src/Views/Hero/Hero.jsx
+++ src/Views/Hero/Hero.jsx
@@ -15,9 +15,9 @@ const Hero = () => {
       {/* <div className={Styles.Hero_Board}></div> */}
       <div className={Styles.Content_Box}>
         <p className={Styles.Small_Text}>Hi, I'm</p>
-        <h1 className={Styles.Large_Text}>
+        <div className={Styles.Large_Text}>
           Daniel <span className={Styles.Red_R}>R</span>ook
-        </h1>
+        </div>
         <p className={Styles.Small_Text}>React Developer</p>
         {/* <button className={Styles.Contact_Button}>Contact</button> */}
       </div>
@@ -25,27 +25,48 @@ const Hero = () => {
       <div className={Styles.Info}>
         <div className={Styles.Info_Section}>
           <h3 className={Styles.Title}>BIO</h3>
-          <p className={Styles.Bio_Text}>
-          Software engineer with 1 year overall Web Development experience. As a Junior Web Developer and am versed in: Html, CSS & JavaScript. However I am skilled in utilizing React JS with a variety of libraries and APIs to solve problems and create websites. Currently I am focused on becoming a valuable integration to any team I am on to develop projects that align with modern UI/UX standards.
-          </p>
+          <div className={Styles.Bio_Text}>
+            Software engineer with 1 year overall Web Development experience. As
+            a Junior Web Developer and am versed in: Html, CSS & JavaScript.
+            However I am skilled in utilizing React JS with a variety of
+            libraries and APIs to solve problems and create websites. Currently
+            I am focused on becoming a valuable integration to any team I am on
+            to develop projects that align with modern UI/UX standards.
+          </div>
         </div>
 
         <div className={Styles.Info_Section}>
           <h3 className={Styles.Title}>On The Web</h3>
           <div className={Styles.Web_Profiles}>
-            <a target={"_blank"} href="https://www.linkedin.com/in/dev-rook/" className={Styles.WebLink}>
+            <a
+              target={"_blank"}
+              href="https://www.linkedin.com/in/dev-rook/"
+              className={Styles.WebLink}
+            >
               <LinkedInIcon />
               Daniel Brown
             </a>
-            <a target={"_blank"} href="https://github.com/Dev-Rook" className={Styles.WebLink}>
+            <a
+              target={"_blank"}
+              href="https://github.com/Dev-Rook"
+              className={Styles.WebLink}
+            >
               <GitHubIcon />
               Dev-Rook
             </a>
-            <a target={"_blank"} href="https://twitter.com/Dev_Rook" className={Styles.WebLink}>
+            <a
+              target={"_blank"}
+              href="https://twitter.com/Dev_Rook"
+              className={Styles.WebLink}
+            >
               <TwitterIcon />
               Dev_Rook
             </a>
-            <a target={"_blank"} href="https://rxresu.me/dev.rook121/developer-resume" className={Styles.WebLink}>
+            <a
+              target={"_blank"}
+              href="https://rxresu.me/dev.rook121/developer-resume"
+              className={Styles.WebLink}
+            >
               <InsertLinkIcon />
               Developer Resume
             </a>
@@ -56,19 +77,39 @@ const Hero = () => {
           <h3 className={Styles.Title}>Technologies Used</h3>
           <div className={Styles.Tech}>
             <div className={Styles.Tech_Card}>
-              <img src={require("../../Assets/Icons/Html-Icon.png")} alt="" className={Styles.Tech_Image} />
+              <img
+                src={require("../../Assets/Icons/Html-Icon.png")}
+                alt=""
+                className={Styles.Tech_Image}
+              />
             </div>
             <div className={Styles.Tech_Card}>
-              <img src={require("../../Assets/Icons/Css-Icon.png")} alt="" className={Styles.Tech_Image} />
+              <img
+                src={require("../../Assets/Icons/Css-Icon.png")}
+                alt=""
+                className={Styles.Tech_Image}
+              />
             </div>
             <div className={Styles.Tech_Card}>
-              <img src={require("../../Assets/Icons/Scss-Icon.png")} alt="" className={Styles.Tech_Image} />
+              <img
+                src={require("../../Assets/Icons/Scss-Icon.png")}
+                alt=""
+                className={Styles.Tech_Image}
+              />
             </div>
             <div className={Styles.Tech_Card}>
-              <img src={require("../../Assets/Icons/JavaScript-Icon.png")} alt="" className={Styles.Tech_Image} />
+              <img
+                src={require("../../Assets/Icons/JavaScript-Icon.png")}
+                alt=""
+                className={Styles.Tech_Image}
+              />
             </div>
             <div className={Styles.Tech_Card}>
-              <img src={require("../../Assets/Icons/React-Icon.png")} alt="" className={Styles.Tech_Image} />
+              <img
+                src={require("../../Assets/Icons/React-Icon.png")}
+                alt=""
+                className={Styles.Tech_Image}
+              />
             </div>
           </div>
         </div>
